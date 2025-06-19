de9864f48be5301cf63a238de042fc56b801fbe1
Holly <hollywhite369@gmail.com>
2025-06-19 16:58:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,8 +75,8 @@ base = alt.Chart(world_map).mark_geoshape(
 points = alt.Chart(filtered).mark_circle(opacity=0.7).encode(
     longitude='longitude:Q',
     latitude='latitude:Q',
-    size=alt.Size('intensity:Q', scale=alt.Scale(type='sqrt', range=[10, 600]), legend=None),
-    color=alt.Color('intensity:Q', scale=alt.Scale(scheme='viridis')),
+    size=alt.value(60),
+    color=alt.Color('intensity:Q', scale=alt.Scale(scheme='viridis'), legend = alt.Legend(title = 'Intensity"),
     tooltip=['location:N', 'time:T', 'intensity:Q']
 )
 
